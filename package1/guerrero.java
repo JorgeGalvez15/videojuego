b350fb1850a4102c58fcf5d26f376160d1599733
Jorge <jorge15gv@gmail.com>
2018-01-27 11:31:09 -0600

    Intercambio inicial de proyecto

---
+++ package1/guerrero.java
@@ -0,0 +1,33 @@
+
+/**
+ * Write a description of class guerrero here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class guerrero extends personaje
+{
+    // instance variables - replace the example below with your own
+    private int poderAtaque;
+
+    /**
+     * Constructor for objects of class guerrero
+     */
+    public guerrero()
+    {
+        // initialise instance variables
+        poderAtaque = 0;
+    }
+
+    /**
+     * An example of a method - replace this comment with your own
+     * 
+     * @param  y   a sample parameter for a method
+     * @return     the sum of x and y 
+     */
+    public int correr(int distancia, int tiempo)
+    {
+        // put your code here
+        return 0;
+    }
+}
---
+++ package1/personaje.java
@@ -0,0 +1,25 @@
+package package1;
+
+/**
+ * Abstract class personaje - write a description of the class here
+ * 
+ * @author: 
+ * Date: 
+ */
+public abstract class personaje
+{
+    // instance variables - replace the example below with your own
+    int numeroPies;
+
+    /**
+     * An example of a method - replace this comment with your own
+     * 
+     * @param  y    a sample parameter for a method
+     * @return        the sum of x and y 
+     */
+    public void correr(int distacia)
+    {
+        // put your code here
+        
+    }
+}
